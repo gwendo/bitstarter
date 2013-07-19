f0ba667a4ba41d159438d5cf465090ce113dab47
Mikael Lönneberg <mikael.lonneberg@gmail.com>
2013-07-19 20:48:20 +0200

    Pictures and text

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
+var path = require('path');
 
-var app = express.createServer(express.logger());
+var app = express();
 
 var fs = require('fs');
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function(request, response) {
   
   var buffer = fs.readFileSync('index.html');
